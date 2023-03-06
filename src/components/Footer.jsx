bbb3e5c502d46325acc90f70097a1e8f8c9606c7
JamalNizami <jamal.anjum2001@gmail.com>
2023-03-06 22:15:09 +0500

    First commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer>
+      <div>
+        <h1>Neen tech</h1>
+        <p>@Copyright 2023</p>
+      </div>
+
+      <div>
+        <h3>
+            Follow us
+        </h3>
+        <div>
+            <a href="https://www.facebook.com/muhammadjamal.anjum/">Facebook</a>
+            <a href="https://www.instagram.com/ig_imjamal/">Instagram</a>
+            <a href="https://github.com/JamalNizami">Github</a>
+            <a href="https://www.linkedin.com/in/muhammad-jamal-anjum-9b1a26206/">Linkedin</a>
+        </div>
+      </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+  AiFillLinkedin,
+} from "react-icons/ai";
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="home">
+        <main>
+          <h1>Neen Tech</h1>
+          <p>Solutions to all your Problems</p>
+        </main>
+      </div>
+
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            "Welcome to NEEN TECH! We are a team of tech experts dedicated to
+            providing top-quality services to our clients. Our mission is to
+            help businesses and individuals achieve their technological goals by
+            providing innovative solutions and exceptional customer support.
+          </p>
+        </div>
+      </div>
+
+      <div className="home3" id="about">
+        <div>
+          <h1>Who we are?</h1>
+          <br />
+          <p>
+            At <b>NEEN TECH</b>, we are a team of passionate tech enthusiasts
+            dedicated to providing top-notch technology solutions to our
+            clients. With years of experience in the field, and are committed to
+            staying up-to-date with the latest advancements in technology to
+            ensure that we always provide the best solutions for our clients.
+            Our team of experts works closely with each client to understand
+            their unique needs and goals, and develop customized solutions to
+            help them achieve success
+          </p>
+        </div>
+      </div>
+
+      <div className="home4" id="brands">
+        <div>
+          <h1>Brands</h1>
+
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1.25s",
+              }}
+            >
+              <AiFillLinkedin />
+              <p>Linkedin</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
